6ad2c5c4504c49576b50cacf9c6d3f421ca73c58
oKidd <imnotokidd@gmail.com>
2023-11-13 18:16:01 -0300

    commimmtimtimtimtimt

--- client/src/components/ViewPaciente.jsx
+++ client/src/components/ViewPaciente.jsx
@@ -29,11 +29,11 @@ const ViewPaciente = ({ data }) => {
       <div className='row mx-auto p-5 pt-0 mt-0'>
         <div className='row mx-auto col-12 bg-secondary-subtle rounded-bottom border-top border-dark p-5'>
           <h1 className='display-1 fs-1 text-center'>Fichas Paciente</h1>
+          {fichas.map((ficha) => (
+            <FichaPreview key={ficha.id} data={ficha} />
+          ))}
         </div>
       </div>
-      {fichas.map((ficha) => (
-        <FichaPreview key={ficha.id} data={ficha} />
-      ))}
     </div>
   )
 }
